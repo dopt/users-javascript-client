a4c67a9306f9f66e4c34e64f71d19d783f615100
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-04-17 19:42:33 +0000

    Release 0.1.0

---
+++ src/api/resources/index.ts
@@ -0,0 +1,5 @@
+export * as users from "./users";
+export * from "./users/types";
+export * as groups from "./groups";
+export * from "./users/client/requests";
+export * from "./groups/client/requests";
---
+++ src/serialization/resources/index.ts
@@ -0,0 +1,5 @@
+export * as users from "./users";
+export * from "./users/types";
+export * from "./users/client/requests";
+export * as groups from "./groups";
+export * from "./groups/client/requests";
